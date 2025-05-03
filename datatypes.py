5d51a50ff739d49a7f31fd4a70da871971e49215
shakilahammed055 <shakilahammed055@gmail.com>
2025-05-03 23:12:25 +0600

    data types

---
+++ datatypes.py
@@ -0,0 +1,21 @@
+num= 64
+n1 = 10.5
+name= "Shakil Ahammed"
+ch ='a'
+print(type(num))
+print(type(n1))
+print(type(name))
+print(type(ch))
+
+#string formated
+print(f"{num} + {n1} = {num+n1}")
+print("Shakil Ahammed" , end=" ")  # [end = " "] use no new line print and space print
+print("0177######1")
+
+#relation operatoe and boolean operation
+print(num<n1)
+print(num>n1)
+print(num<=n1)
+print(num>=n1)
+print(num==n1)
+print(num!=n1)
---
+++ typeCasting.py
@@ -0,0 +1,12 @@
+num1= input('Enter first number:')
+num2=input("Enter Second number:")
+
+sum = num1 + num2
+
+print('num1+num2 ='+sum)
+
+
+num3=int(input('Enter third number:'))
+num4=int(input('Enter fourth number:'))
+sum = num3+num4
+print('Num3=Num4= ',sum)
